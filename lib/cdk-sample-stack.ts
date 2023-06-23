c5506725f7018779cd4245c1d929253f218a305f
kurazumi@i-system.co.jp <kurazumi@i-system.co.jp>
2023-06-23 03:24:35 +0000

    [Update] ESLint と Prettier を連携

--- lib/cdk-sample-stack.ts
+++ lib/cdk-sample-stack.ts
@@ -1,17 +1,37 @@
-import * as cdk from 'aws-cdk-lib'
-import { Construct } from 'constructs'
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack } from 'aws-cdk-lib'
+import type { StackProps } from 'aws-cdk-lib'
+import type { Construct } from 'constructs'
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
+import * as ecs from 'aws-cdk-lib/aws-ecs'
+import * as ecs_patterns from 'aws-cdk-lib/aws-ecs-patterns'
 
-export class CdkSampleStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class CdkSampleStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'MyVPC', {
+      maxAzs: 3, // Default is all AzZs in region
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkSampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const cluster = new ecs.Cluster(this, 'MyCluster', {
+      vpc: vpc,
+    })
+
+    // Create a load-balanced Fargate service and make it public
+    const albFargate = new ecs_patterns.ApplicationLoadBalancedFargateService(
+      this,
+      'MyFargateService',
+      {
+        cluster: cluster,
+        cpu: 512,
+        desiredCount: 6,
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+        },
+        memoryLimitMiB: 2048, // Default is 512
+        publicLoadBalancer: true, // Default is false
+      },
+    )
+    console.log(albFargate)
   }
-  fafa
 }
